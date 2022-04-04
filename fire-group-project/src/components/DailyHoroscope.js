ec16742335b2f6d8d0a9c4d1a3cd091a752a04c1
omokhele <anne.imomoh@gmail.com>
2022-04-04 12:09:35 -0700

    Horoscope

---
+++ fire-group-project/src/components/DailyHoroscope.js
@@ -0,0 +1,94 @@
+function DailyHoroscope () {
+    return (
+        <div>
+            <h1> Daily Horoscope</h1>
+
+        <header>
+            <h3>Aries Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Aries Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Taurus Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Taurus Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Gemini Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Gemini Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Cancer Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Cancer Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Leo Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Leo Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Virgo Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Virgo Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Libra Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Libra Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Scorpio Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Scorpio Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Sagittarius Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Sagittarius Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Capricon Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Capricon Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Aquarius Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Aquarius Logo"/>
+            </div>
+        </header>
+
+        <header>
+            <h3>Pisces Daily Horoscope</h3>
+            <div>
+                <img src="https://i.pinimg.com/originals/39/c9/6b/39c96b03b654113145954d7d76b9262b.png" alt="Pisces Logo"/>
+            </div>
+        </header>
+
+
+
+        </div>
+    )
+}
