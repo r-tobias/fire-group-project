f8196207af29b016a86f7443aac5f36da94ca20b
Charlton_Rosero <charltonprosero@gmail.com>
2022-04-10 18:58:12 -0400

    Finally displaying data

--- fire-group-project/src/components/DailyHoroscope.js
+++ fire-group-project/src/components/DailyHoroscope.js
@@ -1,8 +1,30 @@
-function DailyHoroscope(){
+import fetchData from "../services/constants"
+import { useState, useEffect} from 'react'
+
+function DailyHoroscope({sign, timeframe}){
+
+const [horoscope, setHoroscope] = useState('');
+
+
+
+
+
+useEffect(  ()=>{
+   const reading = async() =>{
+    const zodiac = await fetchData(sign, timeframe)
+        setHoroscope(zodiac)
+        console.log(horoscope)
+   } 
+   reading()
+
+},[sign, timeframe])
+    
 
     return(
         <div>
+            {horoscope ? <p>reading: {horoscope.description}</p> :<p></p>}
 
+            {horoscope ? <p>date range: {horoscope.date_range}</p> :<p></p>}
         </div>
     )
 
