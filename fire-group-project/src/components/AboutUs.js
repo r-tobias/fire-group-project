833fba23595ca421dbfea74f7009ef5188176263
omokhele <anne.imomoh@gmail.com>
2022-04-13 16:09:43 -0700

    committing files

---
+++ fire-group-project/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+
+
+function AboutUs(){
+
+    return(
+        <div>
+            <h1>Hello We are </h1>
+        </div>
+    )
+
+}
+
+export default AboutUs
---
+++ fire-group-project/src/components/Home.js
@@ -0,0 +1,20 @@
+
+import { NavLink } from 'react-router-dom'
+import fetchData from '../services/constants'
+import { useState, useEffect} from 'react'
+import Button from './Button'
+
+
+function Home () {
+
+
+
+    return(
+        <div>
+            <h1>Welcome to Astrology Hub!</h1>
+          
+        </div>
+    )
+}
+
+export default Home
