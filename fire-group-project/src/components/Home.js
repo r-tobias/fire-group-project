dd53620c4df9f805ebc9aa392f2ad67d83d6c330
omokhele <anne.imomoh@gmail.com>
2022-04-14 06:54:52 -0700

    Dark mode added

--- fire-group-project/src/components/Home.js
+++ fire-group-project/src/components/Home.js
@@ -1,8 +1,8 @@
 
-// import { NavLink } from 'react-router-dom'
-// import fetchData from '../services/constants'
-// import { useState, useEffect} from 'react'
-// import Button from './Button'
+import { NavLink } from 'react-router-dom'
+import fetchData from '../services/constants'
+import { useState, useEffect} from 'react'
+import Button from './Button'
 
 
 function Home () {
@@ -11,7 +11,8 @@ function Home () {
 
     return(
         <div>
-            <h1>Welcome to Astrology Hub!</h1>    
+            <h1>Welcome to Astrology Hub!</h1> 
+            {/* <img class="image" src="https://thehowler.org/wp-content/uploads/2019/11/horoscope-graphic-900x607.png" alt="horoscope image"/>    */}
     
         </div>
     )
