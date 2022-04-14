5ad8c5784e4964c5c57945a696023d44d6da0bb5
omokhele <anne.imomoh@gmail.com>
2022-04-14 08:23:20 -0700

    About us Changes

--- fire-group-project/src/components/AboutUs.js
+++ fire-group-project/src/components/AboutUs.js
@@ -1,13 +1,11 @@
 
-
 function AboutUs(){
 
     return(
-        <div>
+        <div className="aboutUs">
             <div className="imageDiv">
-            <img className="image" src="https://thehowler.org/wp-content/uploads/2019/11/horoscope-graphic-900x607.png" alt="horoscope image"/>
+            <img className="image" src="https://thehowler.org/wp-content/uploads/2019/11/horoscope-graphic-900x607.png" alt="horoscope"/>
             </div>
-
             <div className="textDiv">
             <h1>Horoscopia: noun</h1>
             <h3>/ˈhôrəˌskōpia/</h3>
@@ -15,11 +13,10 @@ function AboutUs(){
             <p>A concatenation of the words Horoscope and Utopia.</p>
             <p>Take a peek into a forecast of your future. Know how your starts align. Arm yourself with knowledge.</p>
             <p>For to ...<em>“To know thyself is the beginning of wisdom.”</em></p>
-            <p>Socrates, <b>469-399 B.C.</b></p>     
+            <p>Socrates,<b>469-399 B.C.</b></p>     
             </div>
         </div>
     )
-
 }
 
 export default AboutUs
