2bb3841fe2ff1792e01cd95811e07bfbf70b709f
Charlton_Rosero <charltonprosero@gmail.com>
2022-04-14 10:40:53 -0400

    saving

--- fire-group-project/src/components/Home.js
+++ fire-group-project/src/components/Home.js
@@ -1,5 +1,5 @@
 
-// import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 // import fetchData from '../services/constants'
 // import { useState, useEffect} from 'react'
 // import Button from './Button'
@@ -8,7 +8,11 @@
 function Home () {
     return(
         <div>
-     
+            <h1>Welcome to Horoscopia!</h1>
+            <p>
+                In Horoscopia, you can get customized reading for yesterday, today, and tomorrow with a few simple clicks. <NavLink to="/zodiac">Click Here </NavLink> to find out what's in store for you today.
+            </p>
+            
         </div>
     )
 }
