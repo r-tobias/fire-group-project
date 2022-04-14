e476fa8b1c571e7a3fdd4bc17cbcd438d2d1eee5
omokhele <anne.imomoh@gmail.com>
2022-04-14 10:34:05 -0700

    About us and history components

--- fire-group-project/src/components/AboutUs.js
+++ fire-group-project/src/components/AboutUs.js
@@ -1,12 +1,19 @@
+import avatar from '../images/avatar.png'
 
 function AboutUs(){
 
     return(
         <div className="aboutUs">
-        <h1>About Us: Rebecca, Charlton, Demmy and Anne</h1>
+            <img className="avatar" src={avatar} alt="avatar"/>
+        <div className="thisIsUs">
+        {/* <h2>About Us: </h2> */}
+        <h1>Rebecca, Charlton, Demmy and Anne</h1>
         <p>The greatest team known alive has come together to create a website that reveals your horoscope using Aztro (a horoscope API)</p>
-        <h3>About Our Horoscope Website:</h3>
+        </div>
+        <div className="ourSite">
+        <h2>About Horoscopia!</h2>
         <p>Our Horoscope Website is designed to tell you your daily horoscope needs. Missed a day? Not to worry as we will have all horoscope readings saved for past (yesterday), present (today), and future (tomorrow). Need to know your crushes horoscope? Do not fret my little stalker, we have added filtering out specific horoscopes so you can check them all and no one will judge ;) </p>
+        </div>
         </div>
     )
 }
--- fire-group-project/src/components/History.js
+++ fire-group-project/src/components/History.js
@@ -9,12 +9,13 @@ function History(){
             </div>
             <div className="textDiv">
             <h1>Horoscopia: noun</h1>
+            <hr></hr>
             <h3>/ˈhôrəˌskōpia/</h3>
-            <h6>noun: horoscope</h6>
+            {/* <h6>noun: horoscope</h6> */}
             <p>A concatenation of the words Horoscope and Utopia.</p>
             <p>Take a peek into a forecast of your future. Know how your starts align. Arm yourself with knowledge.</p>
-            <p>For to ...<em>“To know thyself is the beginning of wisdom.”</em></p>
-            <p>Socrates,<b>469-399 B.C.</b></p>     
+            <p>For... <em><b>“To know thyself is the beginning of wisdom.”</b></em></p>
+            <p><b>Socrates</b> 469-399 B.C.</p>     
             </div>
         </div>
     )
