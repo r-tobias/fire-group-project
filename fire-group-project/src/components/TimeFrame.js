d54422c926e64934b0cbd6bb3bc8ef2bd8eab27e
Rebecca Tobias <rebeccatobias123@gmail.com>
2022-04-15 08:52:48 -0400

    removed br line

--- fire-group-project/src/components/TimeFrame.js
+++ fire-group-project/src/components/TimeFrame.js
@@ -3,7 +3,7 @@ function TimeFrame({time}){
     
     return(
         <div className="timeframes-container">
-             <h1>Choose a Time Frame</h1><br/>
+             <h1>Choose a Time Frame</h1>
             {['yesterday','today', 'tomorrow'].map((timeframes) =>(
                 <button className={timeframes} key={timeframes} onClick={()=> time(timeframes)}>{timeframes}</button>      
             ))}
