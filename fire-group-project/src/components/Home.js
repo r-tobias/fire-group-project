8bf8df441d3f383e40b805597b2e47d7c75e2fd8
DemmyRodriguez <rodriguezdemmy@gmail.com>
2022-04-14 20:53:28 -0400

    Added logo to home page and made it transparent

--- fire-group-project/src/components/Home.js
+++ fire-group-project/src/components/Home.js
@@ -1,4 +1,4 @@
-
+import logo from '../images/logo.png'
 import { NavLink } from 'react-router-dom'
 // import fetchData from '../services/constants'
 // import { useState, useEffect} from 'react'
@@ -7,12 +7,16 @@ import { NavLink } from 'react-router-dom'
 
 function Home () {
     return(
-        <div>
+        <div className="home">
+        <div className="logoDiv">
+            <img className="image" src={logo} alt="logo img"/>
+        </div>
+        <div className="homeText">
             <h1>Welcome to Horoscopia!</h1>
             <p>
                 In Horoscopia, you can get customized reading for yesterday, today, and tomorrow with a few simple clicks. <NavLink to="/zodiac">Click Here </NavLink> to find out what's in store for you today.
             </p>
-            
+        </div>
         </div>
     )
 }
