7d2597b61b635c54aa21e9cf89c5287c501a2bb3
Rebecca Tobias <rebeccatobias123@gmail.com>
2022-04-15 03:19:20 -0400

    changed background image of footer to be the same as navbar and added hover effect for toggle dark mode

--- fire-group-project/src/components/AboutUs.js
+++ fire-group-project/src/components/AboutUs.js
@@ -14,7 +14,7 @@ function AboutUs(){
         </div>
         <hr className="hr"></hr>
         <div className="ourSite">
-        <h2>About Horoscopia!</h2>
+        <h1>About Horoscopia!</h1>
         <p>Our Horoscope Website is designed to tell you your daily horoscope needs. Missed a day? Not to worry as we will have all horoscope readings saved for past (yesterday), present (today), and future (tomorrow). Need to know your crushes horoscope? Do not fret my little stalker, we have added filtering out specific horoscopes so you can check them all and no one will judge. 😉</p>
         </div>
         </div>
